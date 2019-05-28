0db849d3eaa067e68f23ff69d7eb1c36703cd951
Rafael <rafael22fs@gmail.com>
2019-05-28 16:36:11 -0300

    post da api cadastro autores

--- alura-pt-1/cdc-admin/src/App.js
+++ alura-pt-1/cdc-admin/src/App.js
@@ -8,13 +8,29 @@ class App extends Component {
 
   constructor() {
     super();
-    this.state = {lista : []};
+    this.state = {lista :[], nome:'', email:'', senha:''};
+    this.enviaForm = this.enviaForm.bind(this);
+    this.setNome = this.setNome.bind(this);
+    this.setEmail = this.setEmail.bind(this);
+    this.setSenha = this.setSenha.bind(this);
   }  
+
+  setNome(evento){
+    this.setState({nome: evento.target.value});
+  }
+
+  setEmail(evento){
+    this.setState({email: evento.target.value});    
+  }
+
+  setSenha(evento){
+    this.setState({senha: evento.target.value});
+  }
 
   componentDidMount(){
     $.ajax(
       {
-        url:"http://cdc-react.herokuapp.com/api/autores",
+        url:"https://cdc-react.herokuapp.com/api/autores",
         dataType: 'json',
         success:function(resposta){
           this.setState({lista:resposta});
@@ -24,24 +40,24 @@ class App extends Component {
   }
 
   enviaForm(evento){
-    $.ajax(
-        {
-        url:"http//localhost:8080/api/autores",
-        contentType: 'application/json',
-        dataType:'json',
-        type:'post',
-        data: JSON.stringify({nome:'',email:'',senha:''}),
-        success: function(resposta){
-          console.log("enviado com sucesso");
-        },
-        error: function(resposta){
-            console.log("erro");
-        }
-      })
+    evento.preventDefault();    
+    $.ajax({
+      url:'https://cdc-react.herokuapp.com/api/autores',
+      contentType:'application/json',
+      dataType:'json',
+      type:'post',
+      data: JSON.stringify({nome:this.state.nome,email:this.state.email,senha:this.state.senha}),
+      success: function(resposta){
+        console.log("enviado com sucesso");
+      }.bind(this),
+      error: function(resposta){
+        console.log("erro");
+      }      
+    });
   }
 
 
-  
+
   render(){
     return (
       <div id="layout">
@@ -75,15 +91,15 @@ class App extends Component {
                   <form class="pure-form pure-form-aligned" onSubmit={this.enviaForm} method="post">
                     <div class="pure-control-group">
                       <label htmlFor="nome">Nome</label> 
-                      <input id="nome" type="text" name="nome" value=""  />                  
+                      <input id="nome" type="text" name="nome" value={this.state.nome} onChange={this.setNome}/>                    
                     </div>
                     <div class="pure-control-group">
                       <label htmlFor="email">Email</label> 
-                      <input id="email" type="email" name="email" value=""  />                  
+                      <input id="email" type="email" name="email" value={this.state.email} onChange={this.setEmail}/>                  
                     </div>
                     <div class="pure-control-group">
                       <label htmlFor="senha">Senha</label> 
-                      <input id="senha" type="password" name="senha"  />                                      
+                      <input id="senha" type="password" name="senha" value={this.state.senha} onChange={this.setSenha}/>                                      
                     </div>
                     <div class="pure-control-group">                                  
                       <label></label> 
